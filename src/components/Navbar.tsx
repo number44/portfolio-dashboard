7d0a2f3d6e03e8ff970f610be5d648cdb4c4093a
number44 <eldan8600@gmail.com>
2022-01-10 11:05:10 +0100

    xx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,18 @@
 import { Link, NavLink } from 'react-router-dom';
 import Toogle from './Toogle';
 interface PropsI {}
+import { BsChevronDoubleLeft } from 'react-icons/bs';
 const Navbar = ({}: PropsI) => {
 	return (
 		<header>
 			<nav className="dark:bg-gray-800 dark:text-zinc-100 z-20 max-6xl mx-auto bg-white shadow-sm w-full h-16 fixed top-0 flax">
-				<div className="brand h-full flex justify-between px-8  items-center">
-					<NavLink to="/">Home</NavLink>
+				<div className="brand text-2xl h-full flex justify-between px-8  items-center">
+					<div className="flex items-center">
+						<NavLink to="/" className="flex items-center hover:text-primary transition duration-300">
+							<BsChevronDoubleLeft />
+							<div className={`ml-2`}>Home</div>
+						</NavLink>
+					</div>
 					<div className="ml-auto">
 						<Toogle />
 					</div>
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,24 +1,66 @@
 import Box from '../layouts/Box';
 import useStore from '../store/about';
 interface PropsI {}
+import { motion } from 'framer-motion';
+import { Link } from 'react-router-dom';
+import { BsPlusLg } from 'react-icons/bs';
 const Home = ({}: PropsI) => {
 	const aboutPl = useStore((state) => state.aboutPl);
 	const aboutEn = useStore((state) => state.aboutEn);
 	const changePl = useStore((state) => state.changeAboutPl);
-	function createMarkup() {
-		return {
-			__html: aboutPl,
-		};
-	}
 	return (
-		<>
-			<Box>xx</Box>
-			<Box>
-				<div dangerouslySetInnerHTML={createMarkup()}></div>
-			</Box>
-			<div className="h-8"></div>
-			<Box>En : {aboutEn}</Box>
-		</>
+		<div className="grid grid-cols-1 sm:grid-cols-2 gap-3">
+			<motion.div whileHover={{ scale: 0.95 }} className="cursor-pointer">
+				<Link to={`/locations/create`}>
+					<Box>
+						<div className="flex items-center justify-between p-8">
+							<h2 className="text-lg text-slate-800 dark:text-slate-200 ">Create Location</h2>
+							<BsPlusLg />
+						</div>
+					</Box>
+				</Link>
+			</motion.div>
+			<motion.div whileHover={{ scale: 0.95 }} className="cursor-pointer">
+				<Link to={`/rooms/create`}>
+					<Box>
+						<div className="flex items-center justify-between p-8">
+							<h2 className="text-lg text-slate-800 dark:text-slate-200 ">Create Room</h2>
+							<BsPlusLg />
+						</div>
+					</Box>
+				</Link>
+			</motion.div>
+			<motion.div whileHover={{ scale: 0.95 }} className="cursor-pointer">
+				<Link to={`/map/categories`}>
+					<Box>
+						<div className="flex items-center justify-between p-8">
+							<h2 className="text-lg text-slate-800 dark:text-slate-200 ">Create Place Category</h2>
+							<BsPlusLg />
+						</div>
+					</Box>
+				</Link>
+			</motion.div>
+			<motion.div whileHover={{ scale: 0.95 }} className="cursor-pointer">
+				<Link to={`/map/create`}>
+					<Box>
+						<div className="flex items-center justify-between p-8">
+							<h2 className="text-lg text-slate-800 dark:text-slate-200 ">Create Place</h2>
+							<BsPlusLg />
+						</div>
+					</Box>
+				</Link>
+			</motion.div>
+			<motion.div whileHover={{ scale: 0.95 }} className="cursor-pointer">
+				<Link to={`/media`}>
+					<Box>
+						<div className="flex items-center justify-between p-8">
+							<h2 className="text-lg text-slate-800 dark:text-slate-200 ">Media</h2>
+							<BsPlusLg />
+						</div>
+					</Box>
+				</Link>
+			</motion.div>
+		</div>
 	);
 };
 
